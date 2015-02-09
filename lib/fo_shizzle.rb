e20c3dd2ee21561644edd2a9caea8fb7def8f1de
zuriel ebron & Gabriel Puello <student@epicodus.com>
2015-02-09 12:33:11 -0800

    replace s with z

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -3,10 +3,12 @@ class String
 define_method(:fo_shizzle) do
   original = []
   modified = []
-    self
-
-
-
-
-end
+    split_word = self.split("")
+    split_word.each() do |char|
+      if char.==("s")
+        char.replace("z")
+      end
+    end
+    split_word.join()
+  end
 end
